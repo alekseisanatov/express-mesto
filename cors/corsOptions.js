20628c055d711bd1718921b79bedbfb90a57cac9
Aleksei-Mac <alexsanatov@gmail.com>
2021-08-25 18:20:13 +0300

    feat: добавить cors

--- cors/corsOptions.js
+++ cors/corsOptions.js
@@ -2,6 +2,8 @@ const corsOptions = {
   origin: [
     'https://alekseisanatov.students.nomoredomains.work',
     'http://alekseisanatov.students.nomoredomains.work',
+    'https://alekseisanatov.backend.st.nomoredomains.work',
+    'http://alekseisanatov.backend.st.nomoredomains.work',
     'localhost:3000',
   ],
 };
